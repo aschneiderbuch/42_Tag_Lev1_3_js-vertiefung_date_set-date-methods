6a55042ba235c20282e1ad00d13c34e3b1983624
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-09 15:46:03 +0100

    testingPhase

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,28 @@
 console.log("test");
+
+
+let datum = new Date();
+console.log(datum);
+document.write(datum);
+
+datum = new Date(2022, 9, 23, 13,25,11)
+console.log(datum);
+
+datum = new Date(2123, 1, 24, 13, 25, 11);
+console.log(datum);
+datum = new Date(2123, 2,24,12,25,11);
+console.log(datum);
+
+datum = new Date(2123,2, 3, 13, 25, 11);
+console.log(datum);
+
+datum = new Date(2019, 10, 22, 13, 25, 11);
+console.log(datum);
+
+
+
+
+
+
+console.log(datum);
+document.write(datum);
